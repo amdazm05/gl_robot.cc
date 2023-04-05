a0ac154e8574d6a76a4b016a8219b0356113c3fa
amdazm05 <amdali1998@yahoo.com>
2023-04-05 16:32:47 +0500

    feat: updated logic to reading the CSV formatted input file ,@TODO refactor into utilities

--- src/input/robotinputhandler.cc
+++ src/input/robotinputhandler.cc
@@ -15,15 +15,15 @@ std::vector<RobotState> && RobotInputHandler::getRobotStatesFromFile(std::string
     Handle->readfromfile(filename.c_str(),stringBuffer.data(),stringBuffer.size());
     std::string temp = std::string(stringBuffer.data());
     std::istringstream stream(temp);
-    int i =0;
-    while(temp[i]!='\0')
+    while(true)
     {
-        std::getline(stream, temp, ',');
         stream >> state.velxy;
         std::getline(stream, temp, ',');
         stream >> state.yawrate;
-        std::cout<<state.velxy<<" --- "<<state.yawrate<<std::endl;
-        i++;
+        if(!std::getline(stream,temp))
+        {
+            break;
+        }
     }
     return {};
 }
