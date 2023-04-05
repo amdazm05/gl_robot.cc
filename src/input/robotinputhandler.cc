e10cf869e3ee2e8b1059f6d7cadb084665d83d5f
amdazm05 <amdali1998@yahoo.com>
2023-04-05 16:18:04 +0500

    feat & WIP: working on parsing csv's

--- src/input/robotinputhandler.cc
+++ src/input/robotinputhandler.cc
@@ -11,9 +11,19 @@ RobotInputHandler::RobotInputHandler()
 
 std::vector<RobotState> && RobotInputHandler::getRobotStatesFromFile(std::string && filename)
 {
-    //  Read a chunk of 16KB
-    Handle->readfromfile(filename.c_str(),stringBuffer.data(),sizeofBufferToRead);
-    //  process buffer chunks and extract words
-    //  remove redundant buffer chunks
-    return std::move(stateVector);
+    RobotState state;
+    Handle->readfromfile(filename.c_str(),stringBuffer.data(),stringBuffer.size());
+    std::string temp = std::string(stringBuffer.data());
+    std::istringstream stream(temp);
+    int i =0;
+    while(temp[i]!='\0')
+    {
+        std::getline(stream, temp, ',');
+        stream >> state.velxy;
+        std::getline(stream, temp, ',');
+        stream >> state.yawrate;
+        std::cout<<state.velxy<<" --- "<<state.yawrate<<std::endl;
+        i++;
+    }
+    return {};
 }
